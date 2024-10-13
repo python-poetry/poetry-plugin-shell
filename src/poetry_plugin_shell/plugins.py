06b4dd8c673bd2a089754a5c10e09540d8acc2ae
Bartosz Sokorski <b.sokorski@gmail.com>
2024-10-13 22:25:28 +0200

    Remove unnecessary plugin activation overload

--- src/poetry_plugin_shell/plugins.py
+++ src/poetry_plugin_shell/plugins.py
@@ -8,7 +8,6 @@ from poetry_plugin_shell.command import ShellCommand
 
 
 if TYPE_CHECKING:
-    from poetry.console.application import Application
     from poetry.console.commands.command import Command
 
 
@@ -16,11 +15,3 @@ class ShellApplicationPlugin(ApplicationPlugin):
     @property
     def commands(self) -> list[type[Command]]:
         return [ShellCommand]
-
-    def activate(self, application: Application) -> None:
-        # Removing the existing shell command to avoid an error
-        # If you're checking this code out to get inspiration
-        # for your own plugins: DON'T DO THIS!
-        if application.command_loader.has("shell"):
-            del application.command_loader._factories["shell"]
-        super().activate(application=application)
