66ef6664a7616e9508b4a446042a24083a4f015a
Yang Fan <266078689+yangfan-yf-yf@users.noreply.github.com>
2026-08-09 22:03:43 +0800

    chore: prepare for Ruff 0.16

--- tests/conftest.py
+++ tests/conftest.py
@@ -211,8 +211,8 @@ def project_factory(
 
         if install_deps:
             for deps in [dependencies, dev_dependencies]:
-                for name, version in deps.items():
-                    pkg = Package(name, version)
+                for dependency_name, version in deps.items():
+                    pkg = Package(dependency_name, version)
                     repo.add_package(pkg)
                     installed.add_package(pkg)
 
